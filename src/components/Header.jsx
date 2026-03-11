c0a411167bfaa013b75402c48418ea7c8076dd4c
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2026-03-11 00:50:35 -0400

    ui changes and improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 function Header() {
     return(
-        <div section id="header">
+        <><div id="header">
             <h1>Hi, I'm Laksumi</h1>
             <p>Looking for valuable learning opportunites and meaningful connections</p>
-        </div>
+        </div><div class="header-background">
+                <span class="ball"></span>
+                <span class="ball"></span>
+                <span class="ball"></span>
+                <span class="ball"></span>
+                <span class="ball"></span>
+                <span class="ball"></span>
+            </div></>
     );
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,12 @@
 const projects = [
     {
-        title: "Capstone",
-        description: "capstone project",
-        stack: ["python","kali","flask","mongodb"],
+        title: "Design and Development of Comprehensive Security Testing – Automated Penetration Testing Desktop Application (Top 5 Finalist out of 250 ECSE Capstone Students)",
+        description: "Designed and developed a full-stack security testing application that performs automated vulnerability analysis and reports results in real time within 60 seconds. Integrated AES-256–encrypted MongoDB storage and real-time Socket.IO messaging to support secure data management and live vulnerability feedback. Configured Kali Linux OS drivers and dependencies to develop and test scripts that check for SQL Injection and Cross-Site Scripting. Automated penetration testing workflows in a Kali Linux environment, improving testing coverage and execution speed by 80% through scripting and orchestration.",
+        stack: [" Kali Linux", "Python", "Flask", "Electron.js", "MongoDB Atlas", "Socket.IO", "Docker", "Playwright", "SQLMap", "XSSer"],
         github: "github"
     },
     {
-        title: "Capstone",
+        title: "Tesla Stock Trading Simulation – AI/Machine Learning Model",
         description: "capstone project",
         stack: ["python","kali","flask","mongodb"],
         github: "github"
@@ -22,19 +22,20 @@ function Projects() {
     return(
         <section id="projects">
             <h2>My Projects</h2>
-            {projects.map((project) => (
+            <div id="project-container">
+            {projects.map((project, index) => (
                 <div key={project.title} className="project-card">
                     <h3>{project.title}</h3>
                     <p>{project.description}</p>
                     <div className="stack">
-                        {project.stack.map((tech) => (
+                        {project.stack.map((tech, index) => (
                         <span key={tech}>{tech}</span>
                         ))}
                     </div>
                     <a href={project.github}>GitHub →</a>
                 </div>
             ))}
-
+            </div>
         </section>
     );
 }
